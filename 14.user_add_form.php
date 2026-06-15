99bf24a06a73cb6c5e4ecef50cf3b1854b33bc34
kyle0724 <150755011+kyle0724@users.noreply.github.com>
2026-06-15 13:36:02 +0800

    Update 14.user_add_form.php

--- 14.user_add_form.php
+++ 14.user_add_form.php
@@ -2,9 +2,9 @@
     <head><title>新增使用者</title></head>
     <body>
 <?php        
-    error_reporting(0);
+    error_reporting(0);  // 隱藏所有的錯誤報告（避免讓使用者看到網頁報錯的敏感資訊）
     session_start();     // 啟動 Session 機制，這樣才能讀取或儲存跨網頁的使用者登入變數
-    if (!$_SESSION["id"]) {
+    if (!$_SESSION["id"]) {   // 如果沒有 id（代表未登入），顯示提示訊息
         echo "請登入帳號";
         echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
     }
